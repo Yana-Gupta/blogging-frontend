dabe6b5488899499d1b055df82d992ea74bffa87
Yana Gupta <yanagupta897@gmail.com>
2023-05-17 14:34:42 +0530

    initial

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FC } from "react";
+
+const Navbar =  async () => {
+    return(
+        <h1>
+            Hello 
+        </h1>
+    )
+}
+
+
+export default Navbar;
