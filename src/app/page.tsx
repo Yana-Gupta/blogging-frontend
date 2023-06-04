351ebec5d43ef6499511dda92722414cf9301faf
Yana Gupta <yanagupta897@gmail.com>
2023-06-05 01:40:43 +0530

    added sidenav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import React from "react"
-import Header from "@assets/images/Header.png"
 
 import UILoading from "@ui/loading"
 
-import Image from "next/image"
-
 // functions
 import { fetchBlogs } from "@api/blog/index"
+import Link from "next/link"
 
 // icons
 
@@ -22,12 +20,12 @@ const Home = async (): Promise<JSX.Element> => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
       <div className="h-[80vh] w-full" id="header">
-        <div className="h-5/6 flex flex-col justify-center px-12 lg:px-32 items-start">
-          <h2 className="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-sams">
-            Read 1000+ blogs on various topics from our community
+        <div className="h-5/6 flex flex-col justify-center px-12 md:px-16 lg:px-18 xl:px-32 items-start">
+          <h2 className="text-4xl sm:text-5xl lg:text-5xl xl:text-7xl font-sams " style={{ lineHeight: "1.22" }}>
+            Read 1000+ blogs on various topics from our community 🌐
           </h2>
-          <button className="text-4xl px-4 py-4 bg-emerald-500 font-bold text-white my-8 rounded-md">
-            Get Started
+          <button className="text-3xl px-7 py-3 bg-emerald-500 font-semibold text-white my-8 rounded-full hover:bg-emerald-600">
+            <Link href="/signin">Get Started</Link>
           </button>
         </div>
       </div>
@@ -39,7 +37,6 @@ const Home = async (): Promise<JSX.Element> => {
         ) : (
           <div className="grid grid-cols-1 gap-6 tablet:grid-cols-2 laptop:grid-cols-2 desktop:grid-cols-3 laptop:gap-x-16 desktop:gap-x-24">
             {blog.map((item: any, index: number) => {
-              // if (index > 20) {
               return (
                 <div
                   className="card w-full h-60 tablet:w-76 laptop:w-100 blog-card light-card hover:shadow-lg"
@@ -52,8 +49,12 @@ const Home = async (): Promise<JSX.Element> => {
                     <p className="pt-2">{`${item.body.substr(0, 140)} ...`}</p>
                   </div>
 
-                  <a href={`/blog/${item._id}`} className="fadeIn" id="btn">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25">
+                  <a
+                    href={`/blog/${item._id}`}
+                    className="fadeIn text-2xl py-2 px-4"
+                    id="btn"
+                  >
+                    <svg viewBox="0 0 25 25">
                       <path
                         fill="#232326"
                         d="m17.5 5.999-.707.707 5.293 5.293H1v1h21.086l-5.294 5.295.707.707L24 12.499l-6.5-6.5z"
