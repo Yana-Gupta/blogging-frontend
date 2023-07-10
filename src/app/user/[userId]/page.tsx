58aff3ce387b54053cf9b442bd29ddce74d18539
Yana Gupta <yanagupta897@gmail.com>
2023-07-10 13:14:42 +0530

    fixing build bug

--- src/app/user/[userId]/page.tsx
+++ src/app/user/[userId]/page.tsx
@@ -6,16 +6,17 @@ import { fetchBlogsForUser } from "@api/blog/index"
 import { useRouter } from "next/navigation"
 
 const user = async (props: any): Promise<JSX.Element> => {
-  var user: object | undefined = {}
-  var userBlogs: object[] | undefined = []
+  var user: any = {}
+  var userBlogs: Array<any> | undefined= []
   const router = useRouter()
 
   await provideUserContext(props?.params?.userId).then((res) => {
     user = res
   })
+
   if (!user) return notFound()
 
-  await fetchBlogsForUser(user?.email).then((res) => {
+  await fetchBlogsForUser(user!.email).then((res) => {
     userBlogs = res
     console.log(userBlogs)
   })
@@ -24,7 +25,7 @@ const user = async (props: any): Promise<JSX.Element> => {
     <div className="my-2 flex flex-col py-2 ">
       <div>
         <h1 className="text-3xl tablet:text-4xl font-semibold py-4 text-center">
-          Welcome Back {user?.name} !
+          Welcome Back {user!.name} !
         </h1>
       </div>
       <div className="px-6 tablet:px-14 laptop:px-18">
@@ -41,13 +42,13 @@ const user = async (props: any): Promise<JSX.Element> => {
                     className="bg-gray-400/25 px-2 py-2 rounded-md"
                   >
                     <div className="px-2 pt-2">
-                      <h1 className="text-xl">{blog?.title}</h1>
-                      <p>{blog?.body.substr(0, 40)} ...</p>
-                      <p>Pusblished On - {blog?.date.substr(0, 10)}</p>
+                      <h1 className="text-xl">{blog!.title}</h1>
+                      <p>{blog!.body.substr(0, 40)} ...</p>
+                      <p>Pusblished On - {blog!.date.substr(0, 10)}</p>
                     </div>
                     <button
                       className="mt-4 hover:bg-gray-400/25 px-2 py-2 rounded-md"
-                      onClick={() => router.push(`/blog/${blog._id}`)}
+                      onClick={() => router.push(`/blog/${blog!._id}`)}
                     >
                       View More
                     </button>
